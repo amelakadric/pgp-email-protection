0a9982c4d9d88ab9265bdf2bc908e64ef1a3a036
Marko Rabat <rabatmarko@gmail.com>
2024-05-28 21:55:03 +0200

    fixed CORS bug

--- main.py
+++ main.py
@@ -5,16 +5,12 @@ app.static_folder = 'static'
 
 @app.route("/")
 def index():
-    return "<h1>Hello World!</h1>"
+    return render_template("index.html")
 
 @app.route("/encr_api", methods=["POST"])
 def encrypt():
     print(request.json)
     return request.json
 
-@app.route("/index.html")
-def get_index():
-    return render_template("index.html")
-    
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
